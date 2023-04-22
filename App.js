82ae2355d4948f7f3e953c3149866a9782be7895
petejades <petejades@gmail.com>
2023-04-22 01:36:47 -0500

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          what is going on
+          Jenkins is a cool automation tool!!!!
         </p>
         <a
           className="App-link"
